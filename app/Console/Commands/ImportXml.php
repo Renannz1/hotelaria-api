fae19b203d17215e6d6a45bd49078bb0bec69a2d
Renan Magalhães <124892507+Renannz1@users.noreply.github.com>
2024-11-03 20:37:07 -0300

    feat: importar dados dos xml hotels e rooms

--- app/Console/Commands/ImportXml.php
+++ app/Console/Commands/ImportXml.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use App\Models\Hotel;
+use App\Models\Room;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
 use SimpleXMLElement;
@@ -10,42 +11,63 @@ use SimpleXMLElement;
 class ImportXml extends Command
 {
 
-    protected $signature = 'command:import-xml-data {file}';
-    protected $description = 'Importa dados de um arquivo XML.';
+    protected $signature = 'command:import-xml-data {files*}';
+    protected $description = 'Importa dados de um ou mais arquivo XML.';
 
     public function handle()
     {
-        $file = $this->argument('file');
+        $files = $this->argument('files');
 
-        if(!file_exists($file)){
-            $this->error("o arquivo {$file} nao foi encontrado.");
-            return;
+        foreach ($files as $file) {
+            if(!file_exists($file)){
+                $this->error("o arquivo {$file} nao foi encontrado.");
+                return;
+            }
+            $this->importarDadosXML($file);
         }
+    }
 
+    private function importarDadosXML($file){
         $xmlConteudo = file_get_contents($file);
         $xml = new SimpleXMLElement($xmlConteudo);
 
-        // var_dump($xmlConteudo);
-        // var_dump($xml);
-
-        foreach ($xml->Hotel as $hotel) {
-            echo $hotel->Name . "\n";
-        }
-
         try {
-            DB::beginTransaction();
-            foreach ($xml->Hotel as $hotel) {
-                $name = (string) $hotel->Name;
+            // importar xml hotels ------------
+            if ($xml->getName() === 'Hotels') {
+                foreach ($xml->Hotel as $hotel) {
+                    $hotelCode = (string) $hotel['id'];
+                    $name = (string) $hotel->Name;
 
-                Hotel::create([
-                    'name' => $name,
-                ]);
+                    Hotel::updateOrCreate(
+                        ['id' => $hotelCode],
+                        ['name' => $name]
+                    );
+                }
+                $this->info("Importação dos dados do arquivo $file realizada com sucesso.");
             }
-            DB::commit();
-            $this->info("Importação dos dados do arquivo XML realizada com sucesso.");
+            // ------------------------------
+
+
+            // importar xml rooms ------------
+            elseif ($xml->getName() === 'Rooms') {
+                foreach ($xml->Room as $room) {
+                    $roomCode = (int) $room['id'];
+                    $name = (string) $room->Name;
+                    $hotelCode = (int) $room['hotelCode'];
+
+                    Room::updateOrCreate(
+                        ['id' => $roomCode],
+                        [
+                            'hotel_id' => $hotelCode,
+                            'name' => $name
+                        ]
+                    );
+                }
+                $this->info("importacao dos dados do arquivo $file realizada com sucesso.");
+            }
+            // ------------------------------
         } catch (\Exception $e) {
-            DB::rollBack();
-            $this->error("Erro ao importar os dados do arquivo XML para o banco de dados.");
+            $this->error("erro ao importar os arquivos xml para o banco de dados." . $e->getMessage());
         }
     }
 }

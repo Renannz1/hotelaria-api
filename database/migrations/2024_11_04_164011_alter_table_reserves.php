7b6ebec3b224b8dae2e488cbb7c7d5236709a4cd
Renan Magalhães <124892507+Renannz1@users.noreply.github.com>
2024-11-04 14:09:46 -0300

    feat: tabela cupons

---
+++ database/migrations/2024_11_04_164011_alter_table_reserves.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cupons', function (Blueprint $table) {
+            $table->id();
+            $table->string('code')->unique();
+            $table->decimal('discount_value', 10, 2);
+            $table->date('expiration_date')->nullable();
+            $table->boolean('status')->default(true);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
+        Schema::table('reserves', function (Blueprint $table) {
+            $table->unsignedBigInteger('coupon_id')->nullable();
+            $table->foreign('coupon_id')->references('id')->on('coupons')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('reserves', function (Blueprint $table) {
+            //
+        });
+    }
+};

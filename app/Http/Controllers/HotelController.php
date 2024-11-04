6897e2ba4fa45d5113fb8f91a429cc37ce275733
Renan Magalhães <124892507+Renannz1@users.noreply.github.com>
2024-11-04 13:12:47 -0300

    docs: swagger HotelController

--- app/Http/Controllers/HotelController.php
+++ app/Http/Controllers/HotelController.php
@@ -5,8 +5,24 @@ namespace App\Http\Controllers;
 use App\Models\Hotel;
 use Illuminate\Http\Request;
 
+/**
+ * @OA\Tag(
+ *     name="Hotels",
+ *     description="Endpoints: Hotels"
+ * )
+ */
 class HotelController extends Controller
 {
+    /**
+     * @OA\Get(
+     *     path="/api/hotels",
+     *     tags={"Hotels"},
+     *     @OA\Response(
+     *         response=200,
+     *         description="Retorna uma lista de hotéis"
+     *     )
+     * )
+     */
     public function index()
     {
         $hotels = Hotel::all();
@@ -16,16 +32,41 @@ class HotelController extends Controller
         ], 200);
     }
 
+    /**
+     * @OA\Post(
+     *     path="/api/hotels",
+     *     tags={"Hotels"},
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\JsonContent(
+     *             type="object",
+     *             @OA\Property(
+     *                 property="name",
+     *                 type="string",
+     *                 description="Nome do hotel"
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=201,
+     *         description="Hotel adicionado com sucesso"
+     *     ),
+     *     @OA\Response(
+     *         response=500,
+     *         description="Falha ao adicionar Hotel"
+     *     )
+     * )
+     */
     public function store(Request $request)
     {
         $request->validate([
             'name' => 'required|string'
         ]);
 
-        $hotel = New Hotel();
+        $hotel = new Hotel();
         $hotel->name = $request->name;
 
-        if($hotel->save()){
+        if ($hotel->save()) {
             return response()->json([
                 'mensagem' => 'Hotel adicionado com sucesso!',
             ], 201);
@@ -36,12 +77,34 @@ class HotelController extends Controller
         }
     }
 
+    /**
+     * @OA\Get(
+     *     path="/api/hotels/{id}",
+     *     tags={"Hotels"},
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         required=true,
+     *         @OA\Schema(
+     *             type="string"
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Retorna os dados de um hotel específico"
+     *     ),
+     *     @OA\Response(
+     *         response=400,
+     *         description="Hotel não encontrado"
+     *     )
+     * )
+     */
     public function show(string $id)
     {
         $hotel = Hotel::find($id);
 
-        if(!$hotel){
-            response()->json([
+        if (!$hotel) {
+            return response()->json([
                 'mensagem' => 'Hotel não encontrado.',
             ], 400);
         }
@@ -51,11 +114,44 @@ class HotelController extends Controller
         ], 200);
     }
 
+    /**
+     * @OA\Put(
+     *     path="/api/hotels/{id}",
+     *     tags={"Hotels"},
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         required=true,
+     *         @OA\Schema(
+     *             type="string"
+     *         )
+     *     ),
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\JsonContent(
+     *             type="object",
+     *             @OA\Property(
+     *                 property="name",
+     *                 type="string",
+     *                 description="Nome do hotel"
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Hotel atualizado com sucesso"
+     *     ),
+     *     @OA\Response(
+     *         response=400,
+     *         description="Hotel não encontrado"
+     *     )
+     * )
+     */
     public function update(Request $request, string $id)
     {
         $hotel = Hotel::find($id);
 
-        if(!$hotel){
+        if (!$hotel) {
             return response()->json([
                 'mensagem' => 'Hotel não encontrado.',
             ], 400);
@@ -73,6 +169,28 @@ class HotelController extends Controller
         ], 200);
     }
 
+    /**
+     * @OA\Delete(
+     *     path="/api/hotels/{id}",
+     *     tags={"Hotels"},
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         required=true,
+     *         @OA\Schema(
+     *             type="string"
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Hotel deletado com sucesso"
+     *     ),
+     *     @OA\Response(
+     *         response=400,
+     *         description="Hotel não encontrado"
+     *     )
+     * )
+     */
     public function destroy(string $id)
     {
         $hotel = Hotel::find($id);

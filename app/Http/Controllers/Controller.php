82325df99e99d0ee0556cf25fa8ebec708764f66
Renan Magalhães <124892507+Renannz1@users.noreply.github.com>
2024-11-04 13:22:19 -0300

    docs: swagger RoomController

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,7 +6,7 @@ namespace App\Http\Controllers;
  * @OA\Info(
  *      version="1.0.0",
  *      title="API FOCO",
- *      description="API FOCO",
+ *      description="Documentação dos endpoints da API",
  *      @OA\Contact(
  *          email="renanpereirareis@gmail.com"
  *      ),
--- app/Http/Controllers/RoomController.php
+++ app/Http/Controllers/RoomController.php
@@ -6,9 +6,24 @@ use App\Models\Hotel;
 use App\Models\Room;
 use Illuminate\Http\Request;
 
+/**
+ * @OA\Tag(
+ *     name="Rooms",
+ *     description="Endpoints: Rooms"
+ * )
+ */
 class RoomController extends Controller
 {
-
+    /**
+     * @OA\Get(
+     *     path="/api/rooms",
+     *     tags={"Rooms"},
+     *     @OA\Response(
+     *         response=200,
+     *         description="Retorna uma lista de quartos"
+     *     )
+     * )
+     */
     public function index()
     {
         $rooms = Room::all();
@@ -18,6 +33,40 @@ class RoomController extends Controller
         ], 200);
     }
 
+    /**
+     * @OA\Post(
+     *     path="/api/rooms",
+     *     tags={"Rooms"},
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\JsonContent(
+     *             type="object",
+     *             @OA\Property(
+     *                 property="hotel_id",
+     *                 type="integer",
+     *                 description="ID do hotel"
+     *             ),
+     *             @OA\Property(
+     *                 property="name",
+     *                 type="string",
+     *                 description="Nome do quarto"
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=201,
+     *         description="Quarto criado com sucesso"
+     *     ),
+     *     @OA\Response(
+     *         response=400,
+     *         description="Hotel não encontrado"
+     *     ),
+     *     @OA\Response(
+     *         response=500,
+     *         description="Falha ao criar quarto"
+     *     )
+     * )
+     */
     public function store(Request $request)
     {
         $request->validate([
@@ -37,11 +86,33 @@ class RoomController extends Controller
         ], 201);
     }
 
+    /**
+     * @OA\Get(
+     *     path="/api/rooms/{id}",
+     *     tags={"Rooms"},
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         required=true,
+     *         @OA\Schema(
+     *             type="string"
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Retorna os dados de um quarto específico"
+     *     ),
+     *     @OA\Response(
+     *         response=400,
+     *         description="Quarto não encontrado"
+     *     )
+     * )
+     */
     public function show(string $id)
     {
         $hotel = Hotel::find($id);
 
-        if(!$hotel){
+        if (!$hotel) {
             return response()->json([
                 'mensagem' => 'Quarto não encontrado.',
             ], 400);
@@ -52,16 +123,48 @@ class RoomController extends Controller
         ], 200);
     }
 
+    /**
+     * @OA\Put(
+     *     path="/api/rooms/{id}",
+     *     tags={"Rooms"},
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         required=true,
+     *         @OA\Schema(
+     *             type="string"
+     *         )
+     *     ),
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\JsonContent(
+     *             type="object",
+     *             @OA\Property(
+     *                 property="name",
+     *                 type="string",
+     *                 description="Nome do quarto"
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Quarto atualizado com sucesso"
+     *     ),
+     *     @OA\Response(
+     *         response=400,
+     *         description="Quarto não encontrado"
+     *     )
+     * )
+     */
     public function update(Request $request, string $id)
     {
         $room = Room::find($id);
 
-        if (!$room){
+        if (!$room) {
             return response()->json([
                 'mensagem' => 'Quarto não encontrado.'
             ], 400);
         }
-
 
         $request->validate([
             'name' => 'required|string'
@@ -75,11 +178,33 @@ class RoomController extends Controller
         ]);
     }
 
+    /**
+     * @OA\Delete(
+     *     path="/api/rooms/{id}",
+     *     tags={"Rooms"},
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         required=true,
+     *         @OA\Schema(
+     *             type="string"
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Quarto deletado com sucesso"
+     *     ),
+     *     @OA\Response(
+     *         response=400,
+     *         description="Quarto não encontrado"
+     *     )
+     * )
+     */
     public function destroy(string $id)
     {
         $room = Room::find($id);
 
-        if(!$room){
+        if (!$room) {
             return response()->json([
                 'mensagem' => 'Quarto não encontrado.'
             ], 400);
@@ -92,4 +217,3 @@ class RoomController extends Controller
         ], 200);
     }
 }
-
